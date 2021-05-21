95bd453787ec23618b521064d57188b8c8c3a9e0
Felix Piskunov <joylandraver@gmail.com>
2021-05-21 16:12:08 +0300

    Added antd and styled-components, TransactionsList component and changed main view to show 2 lists

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
+import TransactionsList from './components/TransactionsList';
+
+const Container = styled.div`
+  margin: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 30px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TransactionsList title="Paying" items={[]} />
+      <TransactionsList title="Receiving" items={[]} />
+    </Container>
   );
 }
 
---
+++ src/components/TransactionsList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Table } from 'antd'
+
+/**
+ * List of transactions
+ * @param {object} param0 
+ * @param {string} param0.title
+ * @param {[]} param0.items
+ * @returns 
+ */
+const TransactionsList = ({title, items}) => {
+  return (
+    <div>
+      <h2>
+        {title}
+      </h2>
+      <Table dataSource={items}>
+        <Table.Column title="Counterparty name" dataIndex="counterParty" />
+        <Table.Column title="Amount" dataIndex="amount" />
+      </Table>
+    </div>
+  )
+}
+
+TransactionsList.propTypes = {
+  title: PropTypes.string,
+  items: PropTypes.array
+}
+
+export default TransactionsList
